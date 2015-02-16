f550dc2b9d588ba0154f8eab7b0edd6519e9f569
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-02-15 21:38:19 -0500

    Update Main.php

--- src/HealME/Main.php
+++ src/HealME/Main.php
@@ -14,19 +14,19 @@ class Main extends PluginBase implements Listener{
       $this->getLogger()->info(TEXTFORMAT::RED . "HealME] " . TEXTFORMAT::GREEN . "I'm Enabled! Let's HEAL!");
     }
  }
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-		if($sender instanceof Player) {
-			$player = $sender->getPlayer()->getName();
-			if(strtolower($command->getName('heal'))) {
-			    $healed = $this->getServer()->getPlayerExact($args[1]);
-          $myhealth = $player->getHealth(); //Gets health
-		      $sethealth = $myhealth + 10; //Adds health
-		      $healed->setHealth($sethealth); //Sets health
-		        $healed->sendMessage("HealME] You Have Been Healed By " . $player . "!");
-		          if(empty($args[1])) {
+public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+	if($sender instanceof Player) {
+		$player = $sender->getPlayer()->getName();
+		if(strtolower($command->getName('heal'))) {
+		$healed = $this->getServer()->getPlayerExact($args[1]);
+          	$myhealth = $player->getHealth(); //Gets health
+		$sethealth = $myhealth + 10; //Adds health
+		$healed->setHealth($sethealth); //Sets health
+		$healed->sendMessage("HealME] You Have Been Healed By " . $player . "!");
+			if(empty($args[1])) {
 		            $player->setHealth($sethealth)
 		            $player->sendMessage("HealME] You Have Been Healed!")
-}
-			}
+	}
 		}
 	}
+}
