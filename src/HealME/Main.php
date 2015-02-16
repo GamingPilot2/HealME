360f02706dd113b89aa5556455fea43959997672
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-02-15 22:11:55 -0500

    Update Main.php

--- src/HealME/Main.php
+++ src/HealME/Main.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace HealME;
-
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
@@ -9,6 +7,10 @@ use pocketmine\utils\TextFormat;
 use pocketmine\command\CommandExecuter
 
 class Main extends PluginBase implements Listener{
+    	public function onEnable(){
+      	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+      	$this->getLogger()->info(TEXTFORMAT::RED . "HealME] " . TEXTFORMAT::GREEN . "I'm Enabled! Let's HEAL!");
+    }
 public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
 	if($sender instanceof Player) {
 		$player = $sender->getPlayer()->getName();
