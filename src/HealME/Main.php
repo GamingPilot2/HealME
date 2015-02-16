bb0336073b802f2a1fe7f542569c4cc55a1a9d66
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-02-15 22:15:58 -0500

    Update Main.php

--- src/HealME/Main.php
+++ src/HealME/Main.php
@@ -1,30 +1,31 @@
 <?php
 namespace HealME;
+
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-use pocketmine\command\CommandExecuter
+use pocketmine\command\CommandExecuter;
 
 class Main extends PluginBase implements Listener{
     	public function onEnable(){
       	$this->getServer()->getPluginManager()->registerEvents($this, $this);
       	$this->getLogger()->info(TEXTFORMAT::RED . "HealME] " . TEXTFORMAT::GREEN . "I'm Enabled! Let's HEAL!");
     }
-public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-	if($sender instanceof Player) {
-		$player = $sender->getPlayer()->getName();
-		if(strtolower($command->getName('heal'))) {
-		$healed = $this->getServer()->getPlayerExact($args[1]);
-          	$myhealth = $player->getHealth();
-		$sethealth = $myhealth + 10; 
-		$healed->setHealth($sethealth); 
-		$healed->sendMessage("HealME] You Have Been Healed By " . $player . "!");
-			if(empty($args[1])) {
-		            $player->setHealth($sethealth)
-		            $player->sendMessage("HealME] You Have Been Healed!")
-				}else{
-		            		$player->setHealth($sethealth)
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		if($sender instanceof Player) {
+			$player = $sender->getPlayer()->getName();
+			if(strtolower($command->getName('heal'))) {
+			$healed = $this->getServer()->getPlayerExact($args[1]);
+          		$myhealth = $player->getHealth();
+			$sethealth = $myhealth + 10; 
+			$healed->setHealth($sethealth); 
+			$healed->sendMessage("HealME] You Have Been Healed By " . $player . "!");
+				if(empty($args[1])) {
+		            	$player->setHealth($sethealth)
+		            	$player->sendMessage("HealME] You Have Been Healed!")
+					}else{
+		            		 $player->setHealth($sethealth)
 		        		 $player->sendMessage("HealME] You Have Been Healed!")
 				}
 	}
